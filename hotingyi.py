e1a4370543c9e0d524825fd0e3598c51c362d8c3
hty0731 <155037911+hty0731@users.noreply.github.com>
2023-12-27 22:01:03 -0500

    Add files via upload

---
+++ hotingyi.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import time
+
+import streamlit as st
+
+import numpy as np
+import pandas as pd
+
+
+# In[2]:
+
+st.title('Tingyi Ho')
+
+
+
+
